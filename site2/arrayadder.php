aa361ddf0d03faaca1b542c38b7b5841575c0e44
taco1945 <taco.smits@live.nl>
2015-05-11 20:44:15 +0200

    reversals engaged

--- site2/arrayadder.php
+++ site2/arrayadder.php
@@ -1,7 +1,6 @@
 <?php
 $text = file_get_contents('items.json');
-$ritem = json_decode($text, true);
-$item = array_reverse($ritem);
+$item = json_decode($text, true);
 $id = $_POST['id'];
 $title = $_POST['title'];
 $url = $_POST['url'];
@@ -10,8 +9,10 @@ $time = $_POST['createdtime'];
 $source = "reddit";
 $add = array('id' => $id, 'title' => $title, 'url' => $url, 'thumbnail' => $thumbnail, 'time' => $time, 'source' => $source);
 
-foreach($item as $x => &$x_item) {
+foreach($item as $x => &$rx_item) {
+	$x_item = array_reverse($rx_item);
 	array_push($x_item, $add);
+	$rx_item = array_reverse($x_item);
 }
 
 $jsonstring = json_encode($item);
--- site2/upload.php
+++ site2/upload.php
@@ -95,14 +95,16 @@ $text = file_get_contents('items.json');
 $item = json_decode($text, true);
 $id = $inserted_fid;
 $title = $name;
-$url = "getpicture.php?"+$inserted_fid;
-$thumbnail = "getpicture.php?"+$inserted_fid;
+$url = $inserted_fid;
+$thumbnail = $inserted_fid;
 $time = time();
 $source = "self";
 $add = array('id' => $id, 'title' => $title, 'url' => $url, 'thumbnail' => $thumbnail, 'time' => $time, 'source' => $source);
 
-foreach($item as $x => &$x_item) {
+foreach($item as $x => &$rx_item) {
+	$x_item = array_reverse($rx_item);
 	array_push($x_item, $add);
+	$rx_item = array_reverse($x_item);
 }
 
 $jsonstring = json_encode($item);
